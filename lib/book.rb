dc80a4e88b0989671d489c95c8f5ef3d1679b17e
Diogo Ide <diogoide@gmail.com>
2018-04-16 23:43:03 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,7 +5,11 @@ class Book
 
   attr_accessor :title, :author, :page_count, :genre
 
-  def turn_page(turn_page)
+  def turn_page=(turn_page)
     @turn_page = puts "Flipping the page... wow, you read fast!"
   end
+
+  def turn_page
+    @turn_page
+  end 
 end
